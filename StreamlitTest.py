a22bb0adc1c8ffa7231d5a0d3621b8679379d2e9
Mario <123744623+mario-hermann@users.noreply.github.com>
2023-02-06 13:36:59 +0100

    Update StreamlitTest.py

--- StreamlitTest.py
+++ StreamlitTest.py
@@ -5,10 +5,9 @@ st.write("""
 #Welcome to the print machine
 This is the first Streamlit test
 """)
+a = 1
+b = 2
 def sum(a, b):
     return (a + b)
-
-a = int(input('Enter 1st number: '))
-b = int(input('Enter 2nd number: '))
 
 print(f'Sum of {a} and {b} is {sum(a, b)}')
