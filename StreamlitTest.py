1ba792893b2917aa997d3df4c85ec2687c20b372
Mario <123744623+mario-hermann@users.noreply.github.com>
2023-02-07 10:41:14 +0100

    Update StreamlitTest.py

--- StreamlitTest.py
+++ StreamlitTest.py
@@ -5,6 +5,8 @@ import streamlit as st
 
 #from matplotlib import pyplot as plt
 #from tabulate import tabulate
+import random
+
 import time
 import datetime
 #from pathlib import Path
@@ -16,8 +18,8 @@ time.sleep(3)
 a = st.text_input("Please enter something: ")
 time.sleep(2)
 st.write(a)
-i = 0
+i = random.random(10000000000000000000000000)
 while True:
-  i += 300
+  i += i*i
   st.write("Hallo, dies ist Durchgang " + str(i))
   time.sleep(0.3)
